39d7652db9249f2de25265910db661932144ed29
Chloe Lee <chloe.lee.767@my.csun.edu>
2021-11-23 22:26:54 -0800

    app connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,21 +1,38 @@
-const express = require("express");
-const connectDB = require('./database/connection.js')
-const app = express();
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const router = express.Router();
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express();
+const MongoClient = require('mongodb').MongoClient
 
-dotenv.config();
+const connectionString = 'mongodb+srv://teamrocket:blast0ff@cluster0.gvgcv.mongodb.net/TeamRocketDB?retryWrites=true&w=majority'
 
-app.use("/",router);
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
 
-connectDB();
-app.use(express.json({ extended: false }));
-router.post('/api/questionModel', require('./api/Question'));
+app.listen(3000, function() {
+console.log ('listening on port 3000')
+})
 
-const PORT = process.env.PORT || 3000;
+app.get ('/',(req,res)=>{
+res.sendFile(__dirname+'/index.html')
+})
 
-app.listen(PORT, () => console.log('Server started on port: ' +PORT));
+app.post ('/sendQuestion', (req, res)=>{
 
-module.exports = app;
+    // connect to db
+    MongoClient.connect(connectionString, {useUnifiedTopology: true})
+    .then(client => {
+        console.log('Connected to the db')
+        const db = client.db('TeamRocketDB')
+        const questionsCollection = db.collection('questions')
 
+    // write the request to the the collections
+    questionsCollection.insertOne(req.body)
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.error(error))
+    })
+    
+    res.redirect('/')
+
+})
