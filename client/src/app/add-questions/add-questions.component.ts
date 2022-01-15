096018c9d22afa8a6ce94676fb0b575f3d60a6c4
Saikat Barua <saikat.barua.994@my.csun.edu>
2022-01-14 20:32:30 -0800

    object is how posting to db

--- client/src/app/add-questions/add-questions.component.ts
+++ client/src/app/add-questions/add-questions.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
+import { QuestionsService } from '../questions.service';
 
 @Component({
   selector: 'app-add-questions',
@@ -9,12 +10,14 @@ import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 export class AddQuestionsComponent {
   name = 'Add Question';
   questionForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private questionService: QuestionsService) {
      this.questionForm = this.fb.group({
         name: '',
         questions: this.fb.array([]),
      });
+
   }
+
   questions(): FormArray {
      return this.questionForm.get("questions") as FormArray
   }
@@ -33,4 +36,23 @@ export class AddQuestionsComponent {
   onSubmit() {
      console.log(this.questionForm.value);
   }
+
+  sendQuestionToDB(arrayOfQuestionAndAnswers:Object) {
+   
+   console.log("TESTING THE SEND QUESTION to")
+
+
+   this.questionService.postQuestionToDB(arrayOfQuestionAndAnswers).subscribe((response: any) => {
+
+     console.log(response);
+
+   })
+
+ }
+
+ printHello() {
+      console.log("hello")
+}
+
+
 }
